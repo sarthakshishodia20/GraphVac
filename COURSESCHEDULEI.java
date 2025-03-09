58e03624c55dbec7b5447df6842b762b00dd46af
SARTHAK SHISHODIA <122453870+sarthakshishodia20@users.noreply.github.com>
2025-03-09 10:38:59 +0530

    Create COURSESCHEDULEI.java

---
+++ COURSESCHEDULEI.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());  
+        }
+        for (int[] pre : prerequisites) {
+            int u = pre[1]; 
+            int v = pre[0]; 
+            adj.get(u).add(v);
+        }
+        int[] indegree = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            for (int it : adj.get(i)) {
+                indegree[it]++;
+            }
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+        int count = 0;
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            count++; 
+            for (int it : adj.get(node)) {
+                indegree[it]--;
+                if (indegree[it] == 0) {
+                    q.add(it);
+                }
+            }
+        }
+        return count == numCourses;
+    }
+}
